1d4ef084840bd227883636aea3e93f0af357fecf
Tim Jones <tdjones74021@yahoo.com>
2025-12-17 11:13:47 -0600

    Add solution for problem 30.

---
+++ problem030.py
@@ -0,0 +1,34 @@
+"""
+Problem 30
+https://projecteuler.net/problem=30
+
+Digit Fifth Powers
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+"""
+
+from pprint import pprint
+
+
+def main():
+    nums = list()
+
+    for n in range(2, 1_000_000):
+        digits = list(map(int, list(str(n))))
+        digit_sum = sum([i**5 for i in digits])
+        if n == digit_sum:
+            nums.append(n)
+
+    print(nums)
+    print(sum(nums))
+
+if __name__ == "__main__":
+    main()
